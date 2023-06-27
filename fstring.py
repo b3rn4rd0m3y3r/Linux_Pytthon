1d2dd21e08ee013b6adb04761d48863750b9187b
Bernardo Meyer <55299005+b3rn4rd0m3y3r@users.noreply.github.com>
2023-06-27 14:54:34 -0300

    Add files via upload

---
+++ fstring.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+print('Content-type: text/html;charset=utf-8 \n\n')
+print('<h1>f-strings PYTHON 3</h1>')
+
+Nome = "Aderbal"
+Sobrenome = "Mattos"
+
+print(f"Muito prazer {Nome} da família {Sobrenome}")
+
---
+++ substring1.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+print('Content-type: text/html \n\n')
+print('<h1>Substrings PYTHON 3</h1>')
+
+regua = "0123456789A123456789B"
+print('<h3>String mestra: ' + regua + '</h3>')
+print('<p>String[1:8] :' + regua[1:8] + '</p>')
+print('<p>String[1:-4] :' + regua[1:-4] + '</p>')
+print('<p>String[8:] :' + regua[8:] + '</p>')
+
+
